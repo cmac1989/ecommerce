d4a5d4de30f17a3f12701ae1f24f116f41dbae81
unknown <fstcam@outlook.com>
2022-04-08 14:25:15 -0400

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom'
 import classes from './Header.module.css'
+import { CgShoppingCart } from 'react-icons/cg'
 
 function Header() {
   return (
@@ -7,7 +8,7 @@ function Header() {
         <NavLink to='/home' className={(navData) => navData.isActive ? classes.active : ''}>Home</NavLink>
         <NavLink to='/products' className={(navData) => navData.isActive ? classes.active : ''}>Products</NavLink>
         <NavLink to='/contact' className={(navData) => navData.isActive ? classes.active : ''}>Contact</NavLink>
-        <NavLink to='/cart' className={(navData) => navData.isActive ? classes.active : ''}>Cart</NavLink>
+        <NavLink to='/cart' className={(navData) => navData.isActive ? classes.active : ''}> 0 {<CgShoppingCart />}</NavLink>
     </div>
   )
 }
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React from 'react'
+import classes from './Products.module.css'
+
+const DUMMYPRODUCTS = [
+  {
+    name: 'shoes',
+  },
+]
 
 function Products() {
   return (
-    <div>
-        <h1>Products</h1>
+    <div className={classes.products}>
+      <h1>Products</h1>
+      <div className={classes.productList}>
+        <div className={classes.productItem}>
+          <img src="https://media.istockphoto.com/photos/blue-sneakers-isolated-on-white-background-picture-id1308274455?s=612x612"></img>
+          <div className={classes.productName}>Shoes</div>
+          <div className={classes.productPrice}>CAD $178.99</div>
+          <button className={classes.btn}>Add to Cart</button>
+        </div>
+      
+      <div className={classes.productList}>
+        <div className={classes.productItem}>
+          <img src="https://media.istockphoto.com/photos/blue-sneakers-isolated-on-white-background-picture-id1308274455?s=612x612"></img>
+          <div className={classes.productName}>Shoes</div>
+          <div className={classes.productPrice}>CAD $178.99</div>
+          <button className={classes.btn}>Add to Cart</button>
+        </div>
+      </div>
+      <div className={classes.productList}>
+        <div className={classes.productItem}>
+          <img src="https://media.istockphoto.com/photos/blue-sneakers-isolated-on-white-background-picture-id1308274455?s=612x612"></img>
+          <div className={classes.productName}>Shoes</div>
+          <div className={classes.productPrice}>CAD $178.99</div>
+          <button className={classes.btn}>Add to Cart</button>
+        </div>
+      </div>
+      <div className={classes.productList}>
+        <div className={classes.productItem}>
+          <img src="https://media.istockphoto.com/photos/blue-sneakers-isolated-on-white-background-picture-id1308274455?s=612x612"></img>
+          <div className={classes.productName}>Shoes</div>
+          <div className={classes.productPrice}>CAD $178.99</div>
+          <button className={classes.btn}>Add to Cart</button>
+        </div>
+      </div>
+      </div>
     </div>
   )
 }
