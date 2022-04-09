3ebc2ca2482f05dc19e5b1a383262f3e982c1bbe
backyardcaveman <fstcam@outlook.com>
2022-04-09 12:53:36 -0400

    third commit

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,49 +1,21 @@
 import React from 'react'
 import classes from './Products.module.css'
-
-const DUMMYPRODUCTS = [
-  {
-    name: 'shoes',
-  },
-]
+import productData from '../data/product.json'
 
 function Products() {
   return (
     <div className={classes.products}>
       <h1>Products</h1>
-      <div className={classes.productList}>
-        <div className={classes.productItem}>
-          <img src="https://media.istockphoto.com/photos/blue-sneakers-isolated-on-white-background-picture-id1308274455?s=612x612"></img>
-          <div className={classes.productName}>Shoes</div>
-          <div className={classes.productPrice}>CAD $178.99</div>
-          <button className={classes.btn}>Add to Cart</button>
-        </div>
-      
-      <div className={classes.productList}>
-        <div className={classes.productItem}>
-          <img src="https://media.istockphoto.com/photos/blue-sneakers-isolated-on-white-background-picture-id1308274455?s=612x612"></img>
-          <div className={classes.productName}>Shoes</div>
-          <div className={classes.productPrice}>CAD $178.99</div>
-          <button className={classes.btn}>Add to Cart</button>
-        </div>
-      </div>
-      <div className={classes.productList}>
-        <div className={classes.productItem}>
-          <img src="https://media.istockphoto.com/photos/blue-sneakers-isolated-on-white-background-picture-id1308274455?s=612x612"></img>
-          <div className={classes.productName}>Shoes</div>
-          <div className={classes.productPrice}>CAD $178.99</div>
-          <button className={classes.btn}>Add to Cart</button>
-        </div>
-      </div>
-      <div className={classes.productList}>
-        <div className={classes.productItem}>
-          <img src="https://media.istockphoto.com/photos/blue-sneakers-isolated-on-white-background-picture-id1308274455?s=612x612"></img>
-          <div className={classes.productName}>Shoes</div>
-          <div className={classes.productPrice}>CAD $178.99</div>
-          <button className={classes.btn}>Add to Cart</button>
-        </div>
-      </div>
-      </div>
+        <ul className={classes.productList}>
+          {productData.map((product) => {
+            return <li className={classes.productItem}>
+              <img src={product.image}></img>
+              <div className={classes.productName}>{product.name}</div>
+              <div className={classes.productPrice}>CAD ${product.price}</div>
+              <button className={classes.btn}>Add to Cart</button>
+            </li>
+          })}
+        </ul>
     </div>
   )
 }
